65e824cf1485edb6d5ad107aeea497c502755455
Girik Sanghi <girik2001@gmail.com>
2022-05-12 20:16:34 +0530

    Find Average Marks Coding Ninjas

---
+++ FindAverageMarks.py
@@ -0,0 +1,20 @@
+'''
+Sample Input 1 :
+3 
+4 
+6
+Sample Output 1 :
+4.333333333333333
+
+Sample Input 2 :
+5 
+10 
+5
+Sample Output 2 :
+6.666666666666667
+'''
+
+a=int(input())
+b=int(input())
+c=int(input())
+print((a+b+c)/3)
