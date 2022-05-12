75d77a820652d0121103a2ba20e0edffa9613a01
Girik Sanghi <girik2001@gmail.com>
2022-05-12 20:25:26 +0530

    Square Pattern Coding Ninjas

---
+++ SquarePattern.py
@@ -0,0 +1,26 @@
+'''
+Sample Input 1:
+7
+Sample Output 1:
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+
+Sample Input 1:
+6
+Sample Output 1:
+666666
+666666
+666666
+666666
+666666
+666666
+'''
+
+n=int(input())
+for i in range(n):
+    print(str(n)*n)
